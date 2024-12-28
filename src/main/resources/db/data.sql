49529ea2a9b93eac03c1d5956a4dc54063c16dcb
MKA1500 <maciejkabsch@gmail.com>
2024-12-28 10:10:39 +0100

    Spring Security: bcrypt passwords in db - standard tables

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -4,3 +4,19 @@ INSERT INTO employee (first_name, last_name, email, phone_number) VALUES
     ('Zofia', 'Wiśniewska', 'zofia.wisniewska@interia.pl', '345-678-901'),
     ('Jan', 'Kamiński', 'jan.kaminski@onet.pl', '456-789-012'),
     ('Maria', 'Lewandowska', 'maria.lewandowska@o2.pl', '567-890-123');
+
+-- password for 3 users below is: test1234
+-- encrypted with: https://www.bcryptcalculator.com/encode
+
+INSERT INTO users (username, password, enabled) VALUES
+    ('john', '{bcrypt}$2a$10$WPYI0ymGl77kScZXmsVFleTDd4EeI.9ZzCp7yR.rc53m.TLR12vkO', 1),
+    ('jane', '{bcrypt}$2a$10$WPYI0ymGl77kScZXmsVFleTDd4EeI.9ZzCp7yR.rc53m.TLR12vkO', 1),
+    ('maciej', '{bcrypt}$2a$10$WPYI0ymGl77kScZXmsVFleTDd4EeI.9ZzCp7yR.rc53m.TLR12vkO', 1);
+
+INSERT INTO authorities (username, authority) VALUES
+    ('john', 'ROLE_EMPLOYEE'),
+    ('jane', 'ROLE_EMPLOYEE'),
+    ('jane', 'ROLE_MANAGER'),
+    ('maciej', 'ROLE_EMPLOYEE'),
+    ('maciej', 'ROLE_MANAGER'),
+    ('maciej', 'ROLE_ADMIN');
--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -7,3 +7,18 @@ CREATE TABLE employee (
     email VARCHAR(100),
     phone_number VARCHAR(15)
 );
+
+CREATE TABLE users (
+    username VARCHAR(50) PRIMARY KEY,
+    password VARCHAR(68) NOT NULL,
+    enabled SMALLINT NOT NULL CHECK (enabled IN (0, 1))
+);
+
+CREATE TABLE authorities (
+    username VARCHAR(50) NOT NULL,
+    authority VARCHAR(50) NOT NULL,
+    CONSTRAINT authorities_pk UNIQUE (username, authority),
+    CONSTRAINT authorities_fk FOREIGN KEY (username)
+        REFERENCES users (username) ON DELETE CASCADE
+);
+
